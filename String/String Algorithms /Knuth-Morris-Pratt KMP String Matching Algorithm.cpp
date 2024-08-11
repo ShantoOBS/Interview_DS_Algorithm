b7e451e64caecb1a0349733b1e7181ad4c20656d
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-08-11 20:28:11 +0600

    Update Knuth-Morris-Pratt KMP String Matching Algorithm.cpp

--- String/String Algorithms /Knuth-Morris-Pratt KMP String Matching Algorithm.cpp
+++ String/String Algorithms /Knuth-Morris-Pratt KMP String Matching Algorithm.cpp
@@ -1,5 +1,5 @@
 /*
-    
+    YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=qases-9gOpk
     Company Tags                : MICROSOFT
     GfG Link                    : https://www.geeksforgeeks.org/problems/search-pattern0205/1
 */
