845a0a2ebf652a5296a702501f4de1e2b4634ad3
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-08-23 22:30:06 +0600

    Create Find Kth Rotation.cpp

---
+++ Binary Search/Find Kth Rotation.cpp
@@ -0,0 +1,21 @@
+/*
+    Company Tags                : Amazon, ABCO, Flipkart
+    Question Link               : https://practice.geeksforgeeks.org/problems/rotation4723/1
+*/
+
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    int l = 0, r = n-1;
+	    while(l < r) {
+	        int mid = l + (r-l)/2;
+	        if(arr[mid] < arr[r]) {
+	            r = mid;
+	        } else {
+	            l = mid+1;
+	        }
+	    }
+	    return r;
+	}
+
+};
