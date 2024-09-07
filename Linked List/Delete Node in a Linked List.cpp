bf043ee2de3b2f382f9131fc3b77a74174d9e329
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-09-07 10:13:01 +0600

    Create Delete Node in a Linked List.cpp

---
+++ Linked List/Delete Node in a Linked List.cpp
@@ -0,0 +1,59 @@
+/*         Scroll Down to see JAVA code also      */
+/*
+    Company Tags                 : Apple, Adobe, Microsoft, Samsung
+    Leetcode Link                : https://leetcode.com/problems/delete-node-in-a-linked-list/
+    Similar GfG Link             : https://practice.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
+*/
+
+/**************************************** C++ *****************************/
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+       node->val=node->next->val;
+       node->next=node->next->next;
+        
+    }
+};
+
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* prev = NULL;
+        
+        while(node && node->next) {
+            node->val = node->next->val;
+            prev = node;
+            node = node->next;
+        }
+        delete(node);
+        prev->next = NULL;
+    }
+};
+
+
+/**************************************** JAVA *****************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+
+        while (node != null && node.next != null) {
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        
+        if (node != null) {
+            if (prev != null)
+                prev.next = null;
+            else
+                node = null;
+        }
+    }
+}
