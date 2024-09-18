96ce5a469cc61925799f4b5b067bd5727eaddacc
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-09-18 19:45:47 +0600

    Create Defanging an IP Address.cpp

---
+++ STL/Defanging an IP Address.cpp
@@ -0,0 +1,16 @@
+
+// Leet-Code Link : https://leetcode.com/problems/defanging-an-ip-address/
+
+class Solution {
+public:
+    string defangIPaddr(string address) 
+    {
+        string result; 
+        for (char c : address) 
+        {
+            if (c == '.') result += "[.]"; 
+            else result += c;
+        }
+        return result; 
+    }        
+};
