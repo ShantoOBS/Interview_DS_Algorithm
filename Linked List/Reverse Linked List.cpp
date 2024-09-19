e9bacdcb3e0ef5f1891fd601c868c776758324a6
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-09-19 18:39:55 +0600

    Update Reverse Linked List.cpp

--- Linked List/Reverse Linked List.cpp
+++ Linked List/Reverse Linked List.cpp
@@ -1,71 +1,136 @@
 /*
-      
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=RreHsOfi14w
       Company Tags                : Paytm, VMWare, Zoho, Accolite, Amazon, Microsoft, Samsung, Snapdeal, D-E-Shaw, MakeMyTrip, Teradata, Walmart, 
                                     Goldman Sachs, Intuit, Adobe, SAP Labs, Tejas Network, Cisco, Qualcomm, Cognizant, Mahindra Comviva, IgniteWorld
       Leetcode Link               : https://leetcode.com/problems/reverse-linked-list/
-      
+      GfG Link                    : https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
 */
 
 /********************************************************************** C++ **********************************************************************/
-
-//T.C : O(n)
-//S.C : O(n) 
+//Approach-1 (Using Recursion - I)
+//T.C : O(m+n)
+//S.C : O(1) (Ignoring recursion stack space)
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-
-        if(head==NULL){
-            return head;
-        }
+    
+    ListNode* reverseSolve(ListNode* head) {
         
-        ListNode *temp=head;
-        stack<int>s;
-        while(temp!=NULL){
-            s.push(temp->val);
-            temp=temp->next;
-        }
-
-        ListNode *t=head;
-
-        while(t!=NULL){
-           t->val=s.top();
-           s.pop();
-           t=t->next;
-        }
-
-
-        return head;
-
-
+        if(!head || !head->next)
+            return head;
+        
+        ListNode* last   = reverseSolve(head->next);
+        head->next->next = head;
+        head->next       = NULL;
+            
+        return last;
+        
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        return reverseSolve(head);
     }
 };
 
+
+//Approach-2 (Using Recursio - II)
+//T.C : O(m+n)
+//S.C : O(1) (Ignoring recursion stack space)
+class Solution {
+public:
+    
+    ListNode* reverseHelp(ListNode* head, ListNode* prev) {
+        
+        if(!head)
+            return prev;
+        
+        ListNode* temp = head->next;
+        head->next = prev;
+        
+        return reverseHelp(temp, head);
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        return reverseHelp(head, NULL);
+    }
+};
+
+
+//Approach-3 (Iterative)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        
+        while (head != NULL) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
+
+
+/********************************************************************** JAVA **********************************************************************/
+//Approach-1 (Using Recursion - I)
+//T.C : O(m+n)
+//S.C : O(1) (Ignoring recursion stack space)
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // We start with three pointers:
-        // 1. prev: This will keep track of the previous node we processed (starts as null).
-        // 2. next: This will keep track of the next node to process.
-        // 3. curr: This is the current node we are processing (starts as the head of the list).
+        return reverseSolve(head);
+    }
+
+    private ListNode reverseSolve(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode last = reverseSolve(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return last;
+    }
+}
+
+
+//Approach-2 (Using Recursio - II)
+//T.C : O(m+n)
+//S.C : O(1) (Ignoring recursion stack space)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverseHelp(head, null);
+    }
+
+    private ListNode reverseHelp(ListNode head, ListNode prev) {
+        if (head == null)
+            return prev;
+
+        ListNode temp = head.next;
+        head.next = prev;
+
+        return reverseHelp(temp, head);
+    }
+}
+
+
+//Approach-3 (Iterative)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public ListNode reverseList(ListNode head) {
         ListNode prev = null;
-        ListNode next = null;
-        ListNode curr = head;
 
-        // We loop through the linked list until we reach the end (when curr is null).
-        while (curr != null) {
-            // Store the next node in the list. We need to do this because we will change the current node's next pointer.
-            next = curr.next;
-
-            // Reverse the link. Instead of pointing to the next node, the current node now points to the previous node.
-            curr.next = prev;
-
-            // Move the previous pointer to the current node (since the current node is now processed).
-            prev = curr;
-
-            // Move the current pointer to the next node (to continue processing the rest of the list).
-            curr = next;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
 
-        // When we've processed all nodes, the prev pointer will be at the new head of the reversed list.
         return prev;
     }
 }
