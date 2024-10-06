b61b8d97609600085c82c4b765c6163dd187cd87
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-10-06 22:07:02 +0600

    Create Bulb Switcher.cpp

---
+++ Math/Bulb Switcher.cpp
@@ -0,0 +1,11 @@
+/*
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/bulb-switcher/
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n); //explained in my video link above
+    }
+};
