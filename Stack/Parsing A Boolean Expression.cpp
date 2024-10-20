335d19ffdbc1d3832dc0e7de78a3944d6c310c43
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-10-20 12:20:24 +0600

    Create Parsing A Boolean Expression.cpp

---
+++ Stack/Parsing A Boolean Expression.cpp
@@ -0,0 +1,197 @@
+/*     Scroll below to see JAVA code also   */
+/*
+    Company Tags                : will update later
+    Leetcode Link               : https://leetcode.com/problems/parsing-a-boolean-expression
+*/
+
+
+/********************************************************************** C++ **********************************************************************/
+
+My Code --
+
+//T.C : O(n)
+//S.C : O(n)
+
+class Solution {
+public:
+    bool parseBoolExpr(string e) {
+
+        int n=e.size();
+
+        stack<char>st;
+
+        char ans='f';
+
+
+        for(int i=0;i<n;i++){
+
+            if(e[i]==')'){
+
+                bool t=false,f=false;
+
+               while(!st.empty()){
+
+                    char ch=st.top();
+                     st.pop();
+
+                     if(ch=='(' ||  ch==',')continue;
+
+                     if(ch=='t'){
+                        t=true;
+                     }
+                     if(ch=='f'){
+                        f=true;
+                     }
+
+                     if(ch=='&'){
+                  
+                    if(f){
+                        st.push('f');
+                    }
+                    else {
+                        st.push('t');
+                    }
+                    break;
+
+                   }
+                     if(ch=='|'){
+
+                    if(t){
+                        st.push('t');
+                    }
+                    else{
+                        st.push('f');
+                    }
+
+                    break;
+                  }
+
+                  if(ch=='!'){
+                      if(f){
+                        st.push('t');
+                      }
+                      else {
+                        st.push('f');
+                      }
+
+                      break;
+                  }
+                     
+
+                }
+               
+            }
+            else {
+                st.push(e[i]);
+            }
+
+
+            if(st.size()==1)ans=st.top();
+
+    
+        }
+
+
+        if(ans=='f')return false;
+
+
+                      
+
+        return true;
+        
+    }
+};
+
+
+
+
+
+
+
+//Approach (Using stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    char solveOp(char op, vector<char>& values) {
+        if (op == '!') 
+            return values[0] == 't' ? 'f' : 't';
+    
+        if (op == '&') 
+            return any_of(values.begin(), values.end(), [](char ch) { return ch == 'f'; }) ? 'f' : 't';
+    
+        if (op == '|') 
+            return any_of(values.begin(), values.end(), [](char ch) { return ch == 't'; }) ? 't' : 'f';
+    
+        return 't'; // Unreachable
+    }
+
+    bool parseBoolExpr(string s) {
+        int n = s.size();
+        stack<char> st;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == ',') continue;
+
+            if (s[i] == ')') {
+                vector<char> values;
+                // Gather all values inside the parentheses
+                while (st.top() != '(') {
+                    values.push_back(st.top());
+                    st.pop();
+                }
+                st.pop();  // Remove '('
+                char op = st.top();
+                st.pop();  // Remove the operator
+                st.push(solveOp(op, values));
+            } else {
+                st.push(s[i]);
+            }
+        }
+        return (st.top() == 't');
+    }
+};
+
+
+
+/********************************************************************** C++ **********************************************************************/
+//Approach (Using stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    private char solveOp(char op, List<Character> values) {
+        if (op == '!') 
+            return values.get(0) == 't' ? 'f' : 't';
+
+        if (op == '&') 
+            return values.stream().anyMatch(ch -> ch == 'f') ? 'f' : 't';
+
+        if (op == '|') 
+            return values.stream().anyMatch(ch -> ch == 't') ? 't' : 'f';
+
+        return 't'; // Unreachable
+    }
+
+    public boolean parseBoolExpr(String expression) {
+        Stack<Character> stack = new Stack<>();
+        int n = expression.length();
+        
+        for (int i = 0; i < n; i++) {
+            char c = expression.charAt(i);
+            if (c == ',') continue;
+
+            if (c == ')') {
+                List<Character> values = new ArrayList<>();
+                // Gather all values inside the parentheses
+                while (stack.peek() != '(') {
+                    values.add(stack.pop());
+                }
+                stack.pop();  // Remove '('
+                char op = stack.pop();  // Get the operator
+                stack.push(solveOp(op, values));  // Push the result back
+            } else {
+                stack.push(c);  // Push the character onto the stack
+            }
+        }
+        return stack.peek() == 't';
+    }
+}
