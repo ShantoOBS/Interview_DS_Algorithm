e50f110a4bf07a332719fe05aa71839f4eab243c
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2025-03-20 20:44:05 +0600

    Create K-th Symbol in Grammar.cpp

---
+++ Recursion/K-th Symbol in Grammar.cpp
@@ -0,0 +1,13 @@
+/*
+    Company Tags                : will soon 
+    Leetcode Link               : https://leetcode.com/problems/k-th-symbol-in-grammar/description/
+*/
+
+//NOTE : 
+//-------- This is basically we are doing backtracking using Recursion. (I told you, Recursion is the father of many topics)
+
+
+/************************************************************ C++ ************************************************************/
+//Approach-1
+//T.C : O(2^n)
+//S.C :  O(2^n*length of each subset) to store each subset
