3fa7a0bcc459ac0b1c50bcaf726ee047126fdf2a
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2025-04-14 01:15:54 +0600

    Create Count Good Numbers.cpp

---
+++ Mathematical /Count Good Numbers.cpp
@@ -0,0 +1,92 @@
+/*     Scroll below to see JAVA code    */
+/*
+
+    Company Tags                : will soon update
+    Leetcode Link               : https://leetcode.com/problems/count-good-numbers
+
+*/
+
+
+/****************************************************** C++ **************************************************************/
+//Approach (Using Maths)
+//T.C : O(log(n))
+//S.C : O(1) but note that we use O(log(n)) system stack space of recursion
+class Solution {
+public:
+    const int M = 1e9+7;
+    int findPower(long long a, long long b) {
+        if(b == 0) {
+            return 1;
+        }   
+
+        long long half = findPower(a, b/2);
+        long long result = (half * half) % M;
+
+        if(b % 2 == 1) {
+            result = (result * a) % M;
+        }
+
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        return (long long)findPower(5, (n+1)/2) * findPower(4, n/2) % M;
+    }
+};
+
+//Approach (Using Maths)
+//T.C : O(log(n))
+//S.C : O(1) 
+class Solution {
+public:
+   const int M=1e9+7;
+   int binaryiterative(long long a,long long b){
+    int ans=1;
+    while(b){
+        if(b&1){
+            ans=(ans* 1LL *a) % M;
+        }
+        a=(a * 1LL * a) % M;
+        b>>=1;
+    }
+    return ans;
+  }
+    int countGoodNumbers(long long n) {
+
+       
+       return (long long)binaryiterative(5,(n+1)/2)*binaryiterative(4,n/2) %M;
+        
+    }
+};
+
+
+/****************************************************** JAVA **************************************************************/
+//Approach (Using Maths)
+//T.C : O(log(n))
+//S.C : O(1) but note that we use O(log(n)) system stack space of recursion
+class Solution {
+    static final int M = (int)1e9 + 7;
+
+    private long findPower(long a, long b) {
+        if (b == 0) {
+            return 1;
+        }
+
+        long half = findPower(a, b / 2);
+        long result = (half * half) % M;
+
+        if (b % 2 == 1) {
+            result = (result * a) % M;
+        }
+
+        return result;
+    }
+
+    public int countGoodNumbers(long n) {
+        long oddPositions = (n + 1) / 2;
+        long evenPositions = n / 2;
+
+        long result = (findPower(5, oddPositions) * findPower(4, evenPositions)) % M;
+        return (int) result;
+    }
+}
